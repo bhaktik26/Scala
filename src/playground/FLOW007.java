d0f382ef8a92aaff8016a44f6d91d99a4989d444
bhaktik26 <bhakti.kapadia95@gmail.com>
2019-07-20 22:00:00 +0530

    CODECHEF PROBLEM

--- src/playground/FLOW007.java
+++ src/playground/FLOW007.java
@@ -1,4 +1,21 @@
 package playground;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class FLOW007 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while(t--!=0) {
+            int n = Integer.parseInt(br.readLine());
+            String str = String.valueOf(n);
+            String sb = new StringBuilder(str).reverse().toString();
+            while (sb.startsWith("0")) {
+                sb = sb.replaceFirst("0", "");
+            }
+            System.out.println(sb);
+        }
+    }
 }
